c44ed710e76ed377ae0e4c0cd6bea15dd71f9715
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2026-02-04 02:49:09 -0700

    Layout finished.

--- app/src/main/java/mx/itson/edu/examenu1/MainActivity.kt
+++ app/src/main/java/mx/itson/edu/examenu1/MainActivity.kt
@@ -1,6 +1,8 @@
 package mx.itson.edu.examenu1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // Instanciar los elementos visuales
+        val c=findViewById<EditText>(R.id.etC)
+        val f=findViewById<EditText>(R.id.etF)
+        val k=findViewById<EditText>(R.id.etK)
+        val btnC=findViewById<Button>(R.id.btnGC)
+        val btnF=findViewById<Button>(R.id.btnGF)
+        val btnK=findViewById<Button>(R.id.btnGK)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
